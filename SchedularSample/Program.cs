0bb4105d6846b2babccd35ab9e51392db8e3d7d9
Mahmoud Alaskalany <mragab.c@channels.com.sa>
2019-11-15 01:19:24 +0300

    Add project files.

---
+++ SchedularSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Coravel;
+using Coravel.Scheduling.Schedule;
+using Microsoft.AspNetCore.Builder.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SchedulerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                    {
+                        configApp.SetBasePath(Directory.GetCurrentDirectory());
+                        configApp.AddEnvironmentVariables(prefix: "PREFIX_");
+                        configApp.AddCommandLine(args);
+                    }).ConfigureServices((hostContext, services) =>
+                {
+                    services.AddScheduler();
+                    services.AddTransient<TestInvocable>();
+                })
+                .Build();
+            host.Services.UseScheduler(scheduler =>
+            {
+                scheduler.Schedule<TestInvocable>().EverySecond();
+            });
+
+            host.Run();
+            Console.Read();
+
+        }
+    }
+}
---
+++ SchedularSample/TestInvocable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Coravel.Invocable;
+
+namespace SchedulerSample
+{
+    public class TestInvocable : IInvocable
+    {
+        public TestInvocable()
+        {
+            
+        }
+        public async Task Invoke()
+        {
+             Console.WriteLine("Hello From Invocable");
+        }
+    }
+}
